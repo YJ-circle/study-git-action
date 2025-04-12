0b12c97453016f5fcdf6608d1ca4791984fb5544
thelightway <thelightway@daum.net>
2025-04-13 02:55:55 +0900

    테스트 페이지 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@Controller
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    @ResponseBody
+    public String testPage(){
+        return "Hello World";
     }
 
 }
